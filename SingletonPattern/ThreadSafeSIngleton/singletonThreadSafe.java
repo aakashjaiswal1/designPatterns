d1c16c4c0b4c7e9ec2e0966fb65c01e89b37ecb6
Aakash Jaiswal <aakashjaiswal1>
2020-09-16 12:28:45 +0530

    Singleton Pattern Commits

---
+++ SingletonPattern/ThreadSafeSIngleton/singletonThreadSafe.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/*
+This is  thread safe implementation of Singleton Pattern - Lazy init
+ */
+class LazyInitializationThreadSafe{
+     static class Employee{
+         ArrayList<String> friends = new ArrayList<>();
+        private static  Employee EmployeeInst;
+        private Employee(){}
+        synchronized public static Employee getInstance(){
+            if(EmployeeInst == null){
+                EmployeeInst = new Employee();
+            }
+            return EmployeeInst;
+        }
+        public void addFriend(String name){
+            this.friends.add(name);
+        }
+        public ArrayList getFriends() {
+            return this.friends;
+        }
+    }
+
+    public static void main(String[] args) {
+         // All the frinds of all employees should be commonly displayed
+        Employee e1 = Employee.getInstance();
+        e1.addFriend("aakash");
+        Employee e2 = Employee.getInstance();
+        System.out.println(e2.getFriends());
+    }
+}
